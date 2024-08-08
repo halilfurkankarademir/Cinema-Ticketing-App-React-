0f474ca4966073f8e506c1fbdb9b7911cfd9853d
Halil Furkan <hfurkankarademir@gmail.com>
2024-08-08 13:13:55 +0300

    upt

--- src/pages/payment/PaymentComplete.jsx
+++ src/pages/payment/PaymentComplete.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
+import QRCode from "react-qr-code";
 import jsPDF from "jspdf";
 import "jspdf-barcode";
 import Navbar from "../../components/Navbar";
@@ -11,14 +12,23 @@ import "./PaymentComplete.css";
 const PaymentComplete = () => {
     const date = new Date().toDateString();
     const location = useLocation();
-    const { movieName, showTime, ticketType, ticketCount, auditorium, orderNo ,selectedSeats, firstname , lastname} =
-        location.state || {};
+    const {
+        movieName,
+        showTime,
+        ticketType,
+        ticketCount,
+        auditorium,
+        orderNo,
+        selectedSeats,
+        firstname,
+        lastname,
+    } = location.state || {};
 
     const toastShownRef = useRef(false);
 
     useEffect(() => {
         if (!toastShownRef.current) {
-            toast.success('Payment complete!');
+            toast.success("Payment complete!");
             toastShownRef.current = true;
         }
     }, []);
@@ -26,13 +36,11 @@ const PaymentComplete = () => {
     const generatePDF = () => {
         const doc = new jsPDF();
 
-        
         doc.setFont("helvetica", "bold");
 
         doc.setFontSize(32);
         doc.text("CineWave", 14, 10);
 
-        
         doc.setFont("helvetica", "normal");
         doc.setFontSize(18);
         doc.text("Ticket Details", 14, 22);
@@ -61,7 +69,10 @@ const PaymentComplete = () => {
         <div>
             <Navbar></Navbar>
             <div className="container-fluid payment-complete-page">
-                <h1 className="d-flex align-items-center mb-3" style={{color:'#55C1FF'}}>
+                <h1
+                    className="d-flex align-items-center mb-3"
+                    style={{ color: "#55C1FF" }}
+                >
                     Payment Complete &nbsp; <i className="bi bi-bag-check"></i>
                 </h1>
                 <h4 className="d-flex justify-content-between align-items-center mb-3">
@@ -70,32 +81,63 @@ const PaymentComplete = () => {
                 <ul className="list-group mb-3 w-100 ">
                     <li className="list-group-item d-flex justify-content-between lh-sm bg-dark text-white">
                         <div>
-                            <h6 className="my-0" style={{color:'#0095FF'}}>Order Number</h6>
+                            <h6 className="my-0" style={{ color: "#0095FF" }}>
+                                Order Number
+                            </h6>
                             <small className="">{orderNo}</small>
                         </div>
                     </li>
                     <li className="list-group-item d-flex justify-content-between lh-sm bg-dark text-white">
                         <div>
-                            <h6 className="my-0" style={{color:'#0095FF'}}>Name</h6>
+                            <h6 className="my-0" style={{ color: "#0095FF" }}>
+                                Name
+                            </h6>
                             <small className="">{`${firstname} ${lastname}`}</small>
                         </div>
                     </li>
                     <li className="list-group-item d-flex justify-content-between lh-sm bg-dark text-white">
                         <div>
-                            <h6 className="my-0" style={{color:'#0095FF'}}>Movie Name</h6>
+                            <h6 className="my-0" style={{ color: "#0095FF" }}>
+                                Movie Name
+                            </h6>
                             <small className="">{movieName}</small>
                         </div>
                     </li>
                     <li className="list-group-item d-flex justify-content-between lh-sm bg-dark text-white">
                         <div>
-                            <h6 className="my-0" style={{color:'#0095FF'}}>Showtime</h6>
+                            <h6 className="my-0" style={{ color: "#0095FF" }}>
+                                Showtime
+                            </h6>
                             <small className="">{`${date} | ${showTime}`}</small>
                         </div>
                     </li>
                     <li className="list-group-item d-flex justify-content-between lh-sm bg-dark text-white">
                         <div>
-                            <h6 className="my-0" style={{color:'#0095FF'}}>Seats</h6>
+                            <h6 className="my-0" style={{ color: "#0095FF" }}>
+                                Seats
+                            </h6>
                             <small className="">{`${selectedSeats}`}</small>
+                        </div>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between lh-sm bg-dark text-white">
+                        <div
+                            style={{
+                                height: "auto",
+                                margin: "0 auto",
+                                maxWidth: 128,
+                                width: "100%",
+                            }}
+                        >
+                            <QRCode
+                                size={512}
+                                style={{
+                                    height: "auto",
+                                    maxWidth: "100%",
+                                    width: "100%",
+                                }}
+                                value={`${orderNo}\n${firstname} ${lastname}\n${movieName}\n${date} | ${showTime}\n${selectedSeats}`}
+                                viewBox={`0 0 256 256`}
+                            />
                         </div>
                     </li>
                 </ul>
