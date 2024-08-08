ee56b681a6339d678468a13a123fb9fddd5f41f8
Halil Furkan <hfurkankarademir@gmail.com>
2024-08-08 11:53:09 +0300

    u

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,17 +12,16 @@ import "./HomePage.css";
 
 const HomePage = () => {
     const [movies, setMovies] = useState([]);
-    
+
     const settings = {
-        infinite: false,
+        infinite: true,
         speed: 800,
-        slidesToShow: 1,
+        slidesToShow: 3, 
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000,
         pauseOnHover: false,
+        arrows: true, 
     };
-    
+
     useGSAP(() => {
         gsap.from(".text", 0.8, {
             y: 40,
@@ -42,6 +41,7 @@ const HomePage = () => {
             delay: 2,
         });
     });
+
     useEffect(() => {
         const fetchMovies = async () => {
             try {
@@ -65,7 +65,6 @@ const HomePage = () => {
         <div>
             <Navbar />
             <div className="container homepage-container">
-                
                 <div className="homepage-content">
                     <Link to="/vision">
                         <button className="learnMore" id="buttonHomepage">
@@ -79,21 +78,16 @@ const HomePage = () => {
                 <br />
                 <br />
                 <Slider {...settings}>
-                <div className="container-fluid secondSection">
-                    <h3>Vision Movies</h3>
-                    <div className="row">
-                        {movies.map((movie) => (
-                            <div className="col-md-4 cardHomepage" key={movie.id}>
-                                <Card
-                                    title={movie.title}
-                                    desc={movie.description}
-                                    img={movie.imageUrl}
-                                    movieId={movie.id}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                    {movies.map((movie) => (
+                        <div className="card-slide" key={movie.id}>
+                            <Card
+                                title={movie.title}
+                                desc={movie.description}
+                                img={movie.imageUrl}
+                                movieId={movie.id}
+                            />
+                        </div>
+                    ))}
                 </Slider>
                 <div className="pre-loader">
                     <div className="content">
