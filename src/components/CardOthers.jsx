15bbb2172c9d659cb6f27f0353ba2869e9574a62
Halil Furkan <hfurkankarademir@gmail.com>
2024-08-17 21:34:19 +0300

    Update CardOthers.jsx

--- src/components/CardOthers.jsx
+++ src/components/CardOthers.jsx
@@ -11,7 +11,7 @@ const Card = ({ title, img, movieId }) => {
 
     return (
         <div className="card-container text-center" onClick={handleClick} style={{height:'40vh', cursor:'pointer'}}>
-            <img src={img} className="image-card" alt={title} style={{width:'100%', height:'80%'}} />
+            <img src={img} className="image-card" alt={title} style={{width:'100%', height:'80%', borderRadius:'.5rem'}} />
             <p style={{marginTop: '1rem'}}>{title}</p>
         </div>
     );
