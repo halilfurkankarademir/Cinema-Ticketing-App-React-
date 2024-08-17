4c54d9e5ee823f8149e07465168ef6c877e9654e
Halil Furkan <hfurkankarademir@gmail.com>
2024-08-17 04:34:12 +0300

    Update CardOthers.jsx

--- src/components/CardOthers.jsx
+++ src/components/CardOthers.jsx
@@ -10,8 +10,8 @@ const Card = ({ title, desc, img, movieId , type}) => {
     };
 
     return (
-        <div className="d-flex justify-content-center align-content-center" onClick={handleClick} style={{height:'40vh'}}>
-            <img src={img} className="image-card" alt={title} style={{width:'70%',height:'45vh', cursor:'pointer'}} />
+        <div className="d-flex justify-content-center align-content-center text-center" onClick={handleClick} style={{height:'40vh'}}>
+            <img src={img} className="image-card text-center" alt={title} style={{width:'70%',height:'45vh', cursor:'pointer'}} />
         </div>
     );
 };
