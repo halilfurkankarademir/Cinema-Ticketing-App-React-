80c63897f04895db37055fc733142db2dc76580d
Halil Furkan <hfurkankarademir@gmail.com>
2024-08-24 06:56:07 +0300

    u

--- src/pages/profile/Tickets.jsx
+++ src/pages/profile/Tickets.jsx
@@ -1,26 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../../components/Navbar';
-import { useAuth } from '../../context/auth';
-import { firestore, collection, getDocs, query, where,doc,deleteDoc } from '../../firebase/firebase';
-import { useNavigate } from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
-import './Tickets.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar";
+import { useAuth } from "../../context/auth";
+import {
+    firestore,
+    collection,
+    getDocs,
+    query,
+    where,
+    doc,
+    deleteDoc,
+    updateDoc,
+    increment,
+} from "../../firebase/firebase";
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import "./Tickets.css";
 
 const Tickets = () => {
     const { currentUser, userLoggedIn } = useAuth();
+    const [totalSoldTicket, setTotalSoldTicket] = useState(0);
+    const [totalRevenue, setTotalRevenue] = useState(0);
     const [tickets, setTickets] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!userLoggedIn) {
-            navigate('/login');
+            navigate("/login");
         }
 
         const fetchTickets = async () => {
             if (currentUser) {
                 try {
-                    const reservationsCollection = collection(firestore, "reservations");
-                    const q = query(reservationsCollection, where("email", "==", currentUser.email));
+                    const reservationsCollection = collection(
+                        firestore,
+                        "reservations"
+                    );
+                    const q = query(
+                        reservationsCollection,
+                        where("email", "==", currentUser.email)
+                    );
                     const ticketsSnapshot = await getDocs(q);
                     const ticketsList = ticketsSnapshot.docs.map((doc) => {
                         const data = doc.data();
@@ -33,60 +51,167 @@ const Tickets = () => {
 
                     setTickets(ticketsList);
                 } catch (err) {
-                    console.error('Error fetching tickets:', err);
-                    toast.error('No tickets found!');
+                    console.error("Error fetching tickets:", err);
+                    toast.error("No tickets found!");
                 }
             }
         };
 
+        const fetchDatas = async () => {
+            try {
+                const reservationsCollection = collection(firestore, "datas");
+                const dataSnap = await getDocs(reservationsCollection);
+
+                if (!dataSnap.empty) {
+                    const docData = dataSnap.docs[0].data();
+                    setTotalRevenue(docData.totalRevenue);
+                    setTotalSoldTicket(docData.totalSoldTicketCount);
+                }
+            } catch (err) {
+                console.error("Error fetching data:", err);
+                toast.error("Error fetching data.");
+            }
+        };
+
+        fetchDatas();
         fetchTickets();
     }, [currentUser, userLoggedIn, navigate]);
 
-    const deleteTicket = async (ticketId, timestamp) => {
+    console.log(tickets);
+
+    const deleteTicket = async (ticketId, timestamp, seatsCount) => {
         try {
             const now = new Date().getTime();
             if (timestamp > now) {
                 const ticketDocRef = doc(firestore, "reservations", ticketId);
                 await deleteDoc(ticketDocRef);
-                setTickets(tickets.filter(ticket => ticket.id !== ticketId));
-                toast.success('Ticket cancelled successfully!');
+
+                const revDocRef = doc(firestore, "datas", "totalRevenueDocId");
+                await updateDoc(revDocRef, {
+                    totalRevenue: increment(-seatsCount * 10),
+                });
+                const ticketSoldDocRef = doc(
+                    firestore,
+                    "datas",
+                    "totalSoldTicketsDocId"
+                );
+                await updateDoc(ticketSoldDocRef, {
+                    totalSoldTicketCount: increment(-1),
+                });
+
+                setTickets(tickets.filter((ticket) => ticket.id !== ticketId));
+                toast.success("Ticket cancelled successfully!");
             } else {
-                toast.error('Ticket date expired!');
+                toast.error("Ticket date expired!");
             }
         } catch (err) {
-            console.error('Error deleting ticket:', err);
-            toast.error('Error cancelling ticket.');
+            console.error("Error deleting ticket:", err);
+            toast.error("Error cancelling ticket.");
         }
     };
 
     return (
         <div>
             <Navbar />
-            <div className='container-fluid justify-content-center align-content-center'>
-                <div className='table-responsive ticketTable'>
+            <div className="container-fluid justify-content-center align-content-center">
+                <div className="table-responsive ticketTable">
                     <h3>My Tickets</h3>
-                    <table className='table table-striped'>
+                    <table className="table table-striped">
                         <thead>
                             <tr>
-                                <th style={{backgroundColor:'transparent',color:'#0095ff'}}>Movie</th>
-                                <th style={{backgroundColor:'transparent',color:'#0095ff'}}>Showtime</th>
-                                <th style={{backgroundColor:'transparent',color:'#0095ff'}}>Seats</th>
-                                <th style={{backgroundColor:'transparent',color:'#0095ff'}}>Date</th>
-                                <th style={{backgroundColor:'transparent',color:'#0095ff'}}>Actions</th>
+                                <th
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        color: "#0095ff",
+                                    }}
+                                >
+                                    Movie
+                                </th>
+                                <th
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        color: "#0095ff",
+                                    }}
+                                >
+                                    Showtime
+                                </th>
+                                <th
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        color: "#0095ff",
+                                    }}
+                                >
+                                    Seats
+                                </th>
+                                <th
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        color: "#0095ff",
+                                    }}
+                                >
+                                    Date
+                                </th>
+                                <th
+                                    style={{
+                                        backgroundColor: "transparent",
+                                        color: "#0095ff",
+                                    }}
+                                >
+                                    Actions
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
                             {tickets.length > 0 ? (
                                 tickets.map((ticket) => (
                                     <tr key={ticket.id}>
-                                        <td style={{backgroundColor:'transparent',color:'white'}}>{ticket.movieName}</td>
-                                        <td style={{backgroundColor:'transparent',color:'white'}}>{ticket.showTime}</td>
-                                        <td style={{backgroundColor:'transparent',color:'white'}}>{ticket.seats.join(", ")}</td>
-                                        <td style={{backgroundColor:'transparent',color:'white'}}>{ticket.date}</td>
-                                        <td style={{backgroundColor:'transparent',color:'white'}}>
-                                            <button 
-                                                className='btn btn-danger btn-sm danger-button'
-                                                onClick={() => deleteTicket(ticket.id, ticket.timestamp)}
+                                        <td
+                                            style={{
+                                                backgroundColor: "transparent",
+                                                color: "white",
+                                            }}
+                                        >
+                                            {ticket.movieName}
+                                        </td>
+                                        <td
+                                            style={{
+                                                backgroundColor: "transparent",
+                                                color: "white",
+                                            }}
+                                        >
+                                            {ticket.showTime}
+                                        </td>
+                                        <td
+                                            style={{
+                                                backgroundColor: "transparent",
+                                                color: "white",
+                                            }}
+                                        >
+                                            {ticket.seats.join(", ")}
+                                        </td>
+                                        <td
+                                            style={{
+                                                backgroundColor: "transparent",
+                                                color: "white",
+                                            }}
+                                        >
+                                            {ticket.date}
+                                        </td>
+                                        <td
+                                            style={{
+                                                backgroundColor: "transparent",
+                                                color: "white",
+                                            }}
+                                        >
+                                            <button
+                                                className="btn btn-danger btn-sm danger-button"
+                                                onClick={() =>
+                                                    deleteTicket(
+                                                        ticket.id,
+                                                        ticket.timestamp,
+                                                        ticket.seats.length
+                                                    )
+                                                }
                                             >
                                                 Cancel
                                             </button>
@@ -95,17 +220,19 @@ const Tickets = () => {
                                 ))
                             ) : (
                                 <tr>
-                                    <td colSpan="5" className='text-center bg-transparent text-white p-5'>No tickets available.</td>
+                                    <td
+                                        colSpan="5"
+                                        className="text-center bg-transparent text-white p-5"
+                                    >
+                                        No tickets available.
+                                    </td>
                                 </tr>
                             )}
                         </tbody>
                     </table>
                 </div>
             </div>
-            <Toaster
-                position="top-center"
-                reverseOrder={true}
-            />
+            <Toaster position="top-center" reverseOrder={true} />
         </div>
     );
 };
